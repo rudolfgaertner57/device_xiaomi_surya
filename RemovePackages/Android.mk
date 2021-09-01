2f580e749c31645f9e63985d5840e34ba2363245
dlwlrma123 <alexfinhart@gmail.com>
2021-09-02 06:17:23 +0800

    surya: packages: remove AudioFx * AudioFx conflicts with dirac, and imho audiofx hasn't been update over years..
    Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt AppDirectedSMSService arcore
+LOCAL_OVERRIDES_PACKAGES := AudioFX AmbientSensePrebuilt AppDirectedSMSService arcore
 LOCAL_OVERRIDES_PACKAGES += CarrierSetup Chrome Chrome-Stub ConnMO ConnMetrics
 LOCAL_OVERRIDES_PACKAGES += DCMO DevicePolicyPrebuilt DMService Drive 
 LOCAL_OVERRIDES_PACKAGES += GCS
